046276d4504f354239829d17f05704553c5d2981
Noëlla Zwaneveld <89777909+NoellaZwaneveld@users.noreply.github.com>
2021-10-05 10:21:34 +0200

    input en if-statement
    input en if-statement

---
+++ metenisweten.py
@@ -0,0 +1,13 @@
+input ('Verzoek')
+
+a = input ('Vul een heel getal in ')
+print ('')
+
+b = input ('Vul een heel getal in ')
+print ('')
+
+max = int(a)
+b = int(b)
+
+if max > b :
+    print('a is het grootste getal: ' + str(max))
